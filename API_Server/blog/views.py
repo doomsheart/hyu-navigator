af963fd34683753310ce2d5b38c2635eb390b17d
Joonho Wohn <doomsheart@gmail.com>
2017-11-04 22:41:47 +0900

    json redecorate

--- API_Server/blog/views.py
+++ API_Server/blog/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 import json
 # Create your views here.
@@ -7,14 +8,19 @@ from requests import Response
 
 @csrf_exempt
 def inputData(request):
-    print(request)
+    # print(request)
     if request.method == 'GET':
         # request_data = ((request.body).decode('utf-8'))
         # request_data = json.loads(request_data)
         # content = request['content']
         print('18')
-        print(request)
+        # print(request)
+        content = request.GET.get("start_id")
+        # print(content["start_id"])
+        # print(request["start_id"])
+        print(content)
     # print('sdasds')
     a = "a"
     # return Response(a)
-    return render(request, 'default_page.html')
+    # return render(request, 'default_page.html')
+    return HttpResponse("123123")
