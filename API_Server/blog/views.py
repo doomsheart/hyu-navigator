82f1bcc85844bca8a3b91f108881bf5a3d1f6e0a
Joonho Wohn <doomsheart@gmail.com>
2017-11-04 22:00:23 +0900

    add upperstropy

--- API_Server/blog/views.py
+++ API_Server/blog/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render
 import json
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
+from requests import Response
 
 
 @csrf_exempt
 def inputData(request):
     print(request)
-    if request.method == 'POST':
+    if request.method == 'GET':
         # request_data = ((request.body).decode('utf-8'))
         # request_data = json.loads(request_data)
-        content = request.POST['content']
+        # content = request['content']
         print('18')
-        print(content)
-    print('sdasds')
+        print(request)
+    # print('sdasds')
+    a = "a"
+    # return Response(a)
     return render(request, 'default_page.html')
