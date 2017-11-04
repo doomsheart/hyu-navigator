b4d705d898e8dc701414ced169edd79eaee6a4c3
junsuLime <vfv0121@gmail.com>
2017-11-05 05:32:22 +0900

    middle id 적용 완료

--- navigator/view.py
+++ navigator/view.py
@@ -12,8 +12,12 @@ def hello_hack():
 
 @app.route("/search")
 def search_path():
-    start_id = request.args.get('start_id', '')
-    end_id = request.args.get('end_id', '')
+    start_id = request.args.get('start_id', None)
+    end_id = request.args.get('end_id', None)
+    middle_id = request.args.get('middle_id', None)
 
-    result = maps.path_search(int(start_id), int(end_id))
+    if start_id is None or end_id is None:
+        return json.dumps({"response": "failure"})
+
+    result = maps.path_search(int(start_id), int(end_id), [int(middle_id)])
     return json.dumps(result)
